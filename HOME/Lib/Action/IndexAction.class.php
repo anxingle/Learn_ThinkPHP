726ffc320f3e97a1ad5c979beed5e92fdc0820a1
anxingle <anxingle@163.com>
2015-01-30 12:15:56 +0800

    CURD之ADD
    增加数据

--- HOME/Lib/Action/IndexAction.class.php
+++ HOME/Lib/Action/IndexAction.class.php
@@ -2,36 +2,30 @@
 // 本类由系统自动生成，仅供测试用途
 class IndexAction extends Action {
     public function index(){
-    $person =array(
-        1=>array('name'=>'AnXingle','age'=>26),
-        2=>array('name'=>'MyPan','age'=>27),
-        3=>array('name'=>'Weiwei','age'=>29),
-        4=>array('name'=>'Shuo','age'=>16),
-        5=>array('name'=>'YongYong','age'=>13),
-        6=>array('name'=>'yuan','age'=>2),
-    ); 
-    $this->assign('family',$person);
-    $this->display('Index/abc');
+
 	//$this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p></div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
     }
     public function user1(){
-        //echo "This is user1 function"."<br>";
-        //echo C('nam');
-        //echo "ID is: ".$_GET['id']."<hr>";
-        //trace('nam',C('nam'));
-        //$us = new Model('user');
-        //$dat = $us->select();
-        //dump($dat);
-        //$u = D('user');
-        //$u->getinf();
-        //$da = $u->select();
-        //$u = D('user');
-        //echo $u->strm('i love lord');
-        $u = M();
-        //$da = $u->query('select * from sel_user');
-        $u->execute('update sel_user set id=7 where id=25');
-        echo "<hr>";
-        //dump($da);
+        $d = array(
+            0 => array(
+                'id'=>'4',
+                'name'=>'张景开1',
+                'score'=>'4',
+            ),
+            1 => array(
+                'id'=>'5',
+                'name'=> '王佳琪',
+                'score'=>'4',
+            ),
+            2 => array(
+                'id'=>'6',
+                'name'=>'王老五',
+                'score'=>'4',
+            ),
+        );
+        $siler = new Model('scoreuser');
+        $siler->addAll($d);
+        //echo $siler->getLastSql();
         echo "<hr>"."If you see this ,it turns out you make it"."<hr>";
         $this->display();
     }
